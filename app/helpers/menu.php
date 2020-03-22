a11d76e20eef72571c407d9be67907ffa16d6044
LAPTOP-AR62V6PB\pc <dev@dopethemes.com>
2020-03-22 19:27:25 +0800

    Initial release.

--- app/helpers/menu.php
+++ app/helpers/menu.php
@@ -7,6 +7,21 @@
  */
 function displayMenu( $menulist = array() ) {
     return '<div class="navbar-end">
-                <a class="navbar-item" href="' . BASE_URL . '/privacy-policy">Privacy</a>
+                <strong class="has-text-white">Share</strong>
+                <p class="buttons">
+                    <a href="https://twitter.com/intent/tweet?url=' . BASE_URL . '" target="_blank" rel="nofollow noopener" class="button  is-info">
+                        <span class="icon">
+                            <i class="fab fa-twitter"></i>
+                        </span>
+                        <span>Twitter</span>
+                    </a>
+
+                    <a href="https://www.facebook.com/sharer.php?u=' . BASE_URL . '" target="_blank" rel="nofollow noopener" class="button is-link">
+                        <span class="icon">
+                            <i class="fab fa-facebook"></i>
+                        </span>
+                        <span>Facebook</span>
+                    </a>
+                </p>
             </div>';
 }
